4e20b61683682731c3556126828731fe7d4cb468
Mary Kelly <mk-km@live.com>
2024-02-03 13:11:16 -0500

    example with responsive cards

--- src/components/AlphaCard.jsx
+++ src/components/AlphaCard.jsx
@@ -1,59 +1,49 @@
 import { useState } from "react";
 import { Card, CardBody } from "reactstrap";
 import "../styles/alphaCardStyle.css";
-import {
-  faChevronRight,
-  faChevronLeft,
-} from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AlphaCard = ({ letters, cards }) => {
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(cards);
+	// set initial alphabet array using the data passed via 'letters'
+	const [alphaArr, setAlphaArr] = useState(letters);
 
-  const previous = () => {
-    console.log(start);
-    console.log(end);
-    if ((start < 0) || (start - cards < 0)) {
-      return;
-    } else {
-      setStart(start - cards);
-      setEnd(end - cards);
-    }
-  };
-  const next = () => {
-    console.log(start);
-    console.log(end);
-    if((start > letters.length - 1) || (start + cards > letters.length - 1)){
-      return
-    } else{
-      setStart(start + cards)
-      setEnd(end + cards)
-    }
-  };
+	// The following function creates a new array from current value of alphaArr where it shifts/rotates the existing items in the array
+	// while maintaining their order relative to each other, ultimately repositioning their indecies by N amount.
 
-  return (
-    <>
-      {letters.slice(start, end).map((letter) => {
-        const { id, color } = letter;
-        return (
-          <Card className="card" key={id} style={{ backgroundColor: color }}>
-            <CardBody className="letter">
-              <div>{id}</div>
-            </CardBody>
-          </Card>
-        );
-      })}
-      <div className="button-container">
-        <button onClick={() => previous()}>
-          <FontAwesomeIcon icon={faChevronLeft} size="6x" />
-        </button>
-        <button onClick={() => next()}>
-          <FontAwesomeIcon icon={faChevronRight} size="6x" />
-        </button>
-      </div>
-    </>
-  );
+	// More simply put, this function "paginates" (pseudo-paginates, really) through the data in the array by repositioning each subset or "page"
+	// of data/items to be at the beginning of the array as the user clicks through. The reason/choice for this is related to simplifying the mapping of the cards in the component.
+
+	function modifyAlphaArr(direction = 1) {
+		// the direction parameter "direction" is a number (either 1 or -1) to determine how to shift the array (positive or negative cards value could have been passed in as the param to make the code more concise, but I think this is more clear)
+		const sliceIndex = cards * direction;
+		const updatedArr = [alphaArr.slice(sliceIndex), alphaArr.slice(0, sliceIndex)].flat(); // note: used non-destrucitve/mutative slice method as to not modify the current alphaArr value and then used the flat method which makes a new array (there are reasons why this important)
+
+		setAlphaArr(updatedArr);
+	}
+
+	return (
+		<>
+			{alphaArr.slice(0, cards).map((letter) => {
+				const { id, color } = letter;
+				return (
+					<Card className='card' key={id} style={{ backgroundColor: color }}>
+						<CardBody className='letter'>
+							<div>{id}</div>
+						</CardBody>
+					</Card>
+				);
+			})}
+			<div className='button-container'>
+				<button onClick={() => modifyAlphaArr(-1)}>
+					<FontAwesomeIcon icon={faChevronLeft} size='6x' />
+				</button>
+				<button onClick={() => modifyAlphaArr(1)}>
+					<FontAwesomeIcon icon={faChevronRight} size='6x' />
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default AlphaCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import AlphaCard from "../components/AlphaCard";
 import "../styles/homePageStyle.css";
 import { upperAlpha } from "../components/alphabet";
-let cards = 5;
 const HomePage = () => {
-  let ref = window.innerWidth;
-  console.log(ref)
-  if((ref < 1100) && (ref >= 800)){
-    console.log(ref)
-    cards = 4;
-  } else if(ref < 800){
-    cards = 3;
-  } else if(ref < 600){
-    cards = 2;
-  } else if(ref < 480) {
-    cards = 1;
-  } else {
-    cards = 5;
-  }
+	const [cards, setCards] = useState(5);
 
-  return (
-    <div className="container"> 
-   
-      <div className="card-container">
-        <AlphaCard cards={cards} letters={upperAlpha}/>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		function setScreenCards(screenWidth) {
+			if (screenWidth >= 1100) {
+				setCards(5);
+			} else if (screenWidth >= 800) {
+				setCards(4);
+			} else if (screenWidth >= 600) {
+				setCards(3);
+			} else if (screenWidth >= 480) {
+				setCards(2);
+			} else {
+				setCards(1);
+			}
+		}
+
+		function handleResize(e) {
+			const screenWidth = e.target.innerWidth;
+			setScreenCards(screenWidth);
+		}
+
+		window.addEventListener("resize", handleResize);
+
+		// set cards based on initial screen
+		setScreenCards(window.innerWidth);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	// cards will change responsively on screen resize. I don't know if the UI behaves as expected, but the card numbers should change accordingly.
+	// console.log("CARDS", cards);
+
+	return (
+		<div className='container'>
+			<div className='card-container'>
+				<AlphaCard cards={cards} letters={upperAlpha} />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
